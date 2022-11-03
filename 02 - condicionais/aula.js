266e823b3bf31283272859971fb3beb95366ab04
saquiol <samrj123@gmail.com>
2022-11-03 15:48:15 -0300

    pasta da aula organizada

---
+++ 02 - condicionais/aula.js
@@ -0,0 +1,14 @@
+
+
+const numero = 9;
+const isNumeroPar = (numero % 2) === 0;
+
+if (numero === 0) {
+    console.log('O número é inválido');
+} else if (isNumeroPar) {
+    console.log('O número é Par');
+} else 
+      console.log('O número é Impar');
+
+
+
