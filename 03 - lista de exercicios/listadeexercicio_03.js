da6fc6dbdb572e83656d132628fbf3f230269b52
saquiol <samrj123@gmail.com>
2023-01-25 11:59:44 -0300

    feito com a aula

---
+++ 03 - lista de exercicios/listadeexercicio_03.js
@@ -0,0 +1,23 @@
+/*
+  3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+  Utileze os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado. 
+
+  Código Condição de pagamento: 
+   - À vista Débito, recebe 10% de desconto;
+   - À vista no Dinheiro ou PIX, recebe 15% de desconto;
+   - Em duas vezes, preço normal de etiqueta sem juros;
+   - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+   */
+
+   const precoDaEtiqueta = 100;
+   const opcaoDePagamento = 1;
+
+   if (opcaoDePagamento === 1) {
+    console.log(precoDaEtiqueta - (precoDaEtiqueta * 0.1));
+   } else if (opcaoDePagamento === 2) {
+        console.log(precoDaEtiqueta - (precoDaEtiqueta * 0.15));
+   } else if (opcaoDePagamento === 3) {
+    console.log(precoDaEtiqueta);
+   } else if(opcaoDePagamento === 4) {
+    console.log(precoDaEtiqueta + (precoDaEtiqueta * 0.1));
+    }
